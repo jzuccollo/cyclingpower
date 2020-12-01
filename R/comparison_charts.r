44377329d8630587f118a598d0c03579d23ff664
jz <james.zuccollo@epi.org.uk>
2020-12-01 20:58:35 +0000

    Restructure data to accommodate more files

--- R/comparison_charts.r
+++ R/comparison_charts.r
@@ -1,16 +1,16 @@
 #' compare_ts
 #'
 #' @param rundata tbl
+#' @param runvar chr variable denoting the different runs
 #'
 #' @return
-compare_ts <- function(rundata) {
+compare_ts <- function(rundata, runvar) {
   rundata %>%
-    tidylog::pivot_longer(
-      -timestamp,
-      names_to = "source",
-      values_to = "power"
-    ) %>%
-    ggplot2::ggplot(ggplot2::aes(x = .data$timestamp, y = .data$power, colour = .data$source)) +
+    ggplot2::ggplot(ggplot2::aes(
+      x = .data$timestamp,
+      y = .data$power,
+      colour = .data$power_source
+    )) +
     tidyquant::geom_ma(n = 30, linetype = "solid", size = 1) +
     ggplot2::geom_line(alpha = 0.5) +
     ggplot2::labs(
@@ -19,33 +19,51 @@ compare_ts <- function(rundata) {
       title = "Comparison of power data across sources",
       subtitle = "1-second power and 30-second moving average"
     ) +
-    ggplot2::scale_color_brewer(type = "qual")
+    ggplot2::scale_color_brewer(type = "qual") +
+    ggplot2::facet_wrap(runvar, ncol = 1, scales = "free_x")
 }
 
 #' compare_distribution
 #'
 #' @param rundata tbl
-#' @param pm1 var First power variable
-#' @param pm2 var Second power variable
+#' @param runvar chr variable denoting the different runs
 #'
 #' @return
-compare_distribution <- function(rundata, pm1, pm2) {
-  rundata %>%
+compare_distribution <- function(rundata, runvar) {
+  power_sources <- unique(rundata$power_source)
+  if (length(power_sources) > 2) {
+    stop("Too many power sources to difference.")
+  }
+  pm1 <- rlang::sym(power_sources[[1]])
+  pm2 <- rlang::sym(power_sources[[2]])
+
+  reshaped_power <- rundata %>%
+    tidyr::pivot_wider(names_from = "power_source", values_from = "power") %>%
     tidylog::drop_na() %>%
-    tidylog::mutate(
-      power_diff = {{ pm1 }} - {{ pm2 }},
-      median_power_diff_pc = 100 * stats::median(power_diff) / stats::median({{ pm2 }})
-    ) %>%
-    ggplot2::ggplot(ggplot2::aes(x = power_diff)) +
+    tidylog::mutate(power_diff = {{ pm1 }} - {{ pm2 }})
+
+  median_power <- reshaped_power %>%
+    dplyr::group_by(!!rlang::sym(runvar)) %>%
+    dplyr::summarise(
+      median_power_diff = stats::median(.data$power_diff),
+      median_power_diff_pc = 100 * .data$median_power_diff / stats::median({{ pm2 }})
+    )
+
+  reshaped_power %>%
+    ggplot2::ggplot(ggplot2::aes(x = .data$power_diff)) +
     ggplot2::geom_histogram(binwidth = 2) +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = stats::median(power_diff)), linetype = "dashed") +
+    ggplot2::geom_vline(
+      data = .data$median_power,
+      ggplot2::aes(xintercept = .data$median_power_diff),
+      linetype = "dashed"
+    ) +
     ggplot2::geom_text(
+      data = .data$median_power,
       ggplot2::aes(
-        x = median(power_diff),
+        x = .data$median_power_diff,
         y = -2,
         label = glue::glue(
-          "Median difference: {stats::median(power_diff)} watts \\
-      ({scales::comma(first(median_power_diff_pc))}%)"
+          "Median difference: {median_power_diff} watts ({scales::comma(median_power_diff_pc)}%)"
         )
       ),
       hjust = 0,
@@ -57,7 +75,8 @@ compare_distribution <- function(rundata, pm1, pm2) {
       y = "Frequency",
       title = "Distribution of difference in power",
       subtitle = "1-second differences in power, where power is recorded on both devices"
-    )
+    ) +
+    ggplot2::facet_wrap(runvar, ncol = 1, scales = "free_y")
 }
 
 
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,3 @@
 library(testthat)
-library(cyclingpower)
 
 test_check("cyclingpower")
